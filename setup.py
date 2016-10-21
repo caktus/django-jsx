413cb4af7abd4f6a0ace0488fcd225a74a9b7cf6
Calvin Spealman <calvin@caktusgroup.com>
2016-10-21 19:45:27 -0400

    0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-jsx',
-    version='0.2.1',
+    version='0.2.2',
     author='Calvin Spealman',
     author_email='calvin@caktusgroup.com',
     packages=find_packages(exclude=['sample_project']),
